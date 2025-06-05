e24dcc37c6999f6f4f320be235ca3d4ae5506ffe
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2025-06-05 16:24:43 +0200

    ⚡️(frontend) improve prompt of some actions
    Some answers were a bit too concise or not detailed enough.
    Improve some prompts to get better answers from the AI.

--- src/frontend/apps/impress/src/features/docs/doc-editor/components/AI/useAI.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-editor/components/AI/useAI.tsx
@@ -1,9 +1,39 @@
 import { createOpenAI } from '@ai-sdk/openai';
-import { createAIExtension, createBlockNoteAIClient } from '@blocknote/xl-ai';
+import {
+  createAIExtension,
+  createBlockNoteAIClient,
+  llmFormats,
+} from '@blocknote/xl-ai';
+import { CoreMessage } from 'ai';
 import { useMemo } from 'react';
 
 import { fetchAPI } from '@/api';
 import { Doc } from '@/docs/doc-management';
+
+const systemPrompts: Record<
+  'add-default' | 'add-adding-instruction',
+  CoreMessage
+> = {
+  'add-default': {
+    role: 'system',
+    content: `You are an AI assistant that helps users with their documents.
+    Answer the user prompt in markdown format.
+    Add formatting to the text to make it more readable.`,
+  },
+  'add-adding-instruction': {
+    role: 'system',
+    content: `Keep adding to the document, do not delete or modify existing blocks.`,
+  },
+};
+
+const userPrompts: Record<string, string> = {
+  'continue writing':
+    'Keep writing about the content send in the prompt, expanding on the ideas.',
+  'improve writing':
+    'Improve the writing of the selected text. Make it more professional and clear.',
+  summarize:
+    'Summarize the selected text into a concise paragraph. Add a small summarize title above the text.',
+};
 
 const client = createBlockNoteAIClient({
   baseURL: ``,
@@ -39,6 +69,40 @@ export const useAI = (docId: Doc['id']) => {
       agentCursor: {
         name: 'Albert',
         color: '#8bc6ff',
+      },
+      // Create a custom promptBuilder that extends the default one
+      promptBuilder: async (editor, opts): Promise<Array<CoreMessage>> => {
+        const defaultPromptBuilder = llmFormats.html.defaultPromptBuilder;
+        const isTransform = !!opts.selectedBlocks?.length;
+
+        // Try to catch the action
+        const customPromptMatch = opts.userPrompt.match(/^([^:]+)(?=[:]|$)/);
+
+        if (customPromptMatch?.length) {
+          const promptKey = customPromptMatch[0].trim().toLowerCase();
+
+          if (userPrompts[promptKey]) {
+            const modifiedOpts = {
+              ...opts,
+              userPrompt: userPrompts[promptKey],
+            };
+
+            const prompts = await defaultPromptBuilder(editor, modifiedOpts);
+
+            if (!isTransform) {
+              prompts[0] = systemPrompts['add-default'];
+
+              if (prompts.length > 4) {
+                prompts[4] = systemPrompts['add-adding-instruction'];
+              }
+            }
+
+            return prompts;
+          }
+        }
+
+        // If no custom prompt was found or matched, fall back to the default prompt builder
+        return defaultPromptBuilder(editor, opts);
       },
     });
 
